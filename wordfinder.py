e9cc05f597ed63f668b308f596768c1bdf9857d4
Colin Sidberry <colinsidberry@Colins-MacBook-Pro.local>
2021-09-07 15:57:24 -0700

    wrote initializer and read file methods

--- wordfinder.py
+++ wordfinder.py
@@ -1,2 +1,15 @@
 class WordFinder:
     """Word Finder: finds random words from a dictionary."""
+
+    def __init__(self,file_name):
+        """Open user provide file that has one word per line and print the number of words"""
+        self.file_name = open(file_name)
+        self.word_count = self.read_file()
+        print (f"{self.word_count} words read")
+
+    def read_file(self):
+        """read the user provide file, count the words in the file and return the word count"""
+        word_count = 0
+        for line in self.file_name:
+            word_count += 1
+        return word_count
